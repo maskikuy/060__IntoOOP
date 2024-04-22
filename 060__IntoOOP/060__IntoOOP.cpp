1bdfe8b9d826be877a3e377add7957bac672b177
maskikuy <kproject305@gmail.com>
2024-04-22 14:48:05 +0700

    masukan void output

--- 060__IntoOOP/060__IntoOOP.cpp
+++ 060__IntoOOP/060__IntoOOP.cpp
@@ -6,3 +6,9 @@ class Mahasiswa{
 		string nama;
 		int umur;
 		string jurusan;
+
+		void output() {
+			cout << "Nama : " << nama << endl;
+			cout << "Umur : " << umur << endl;
+			cout << "Jurusan : " << jurusan << endl;
+		}
